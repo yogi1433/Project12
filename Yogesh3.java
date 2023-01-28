f21ad7326c45c23423da369c553d2b121891518f
yogi1433 <parnapalliyogeshhanuman@gmail.com>
2023-01-28 16:25:14 +0530

    moving

---
+++ Yogesh3.java
@@ -0,0 +1,12 @@
+class Yogesh3
+{
+	public static void main(String[] args)
+	{
+		System.out.println(+20+20+" is the answer");
+		System.out.println("the answer is"+20+20);
+		System.out.println("the answer is "+(20+20));
+		System.out.println((1/2)+"is the answer");
+		System.out.println("the answer is "+(1/2.0));
+		System.out.println((25+27-98)+"is the answer");
+	}
+}
